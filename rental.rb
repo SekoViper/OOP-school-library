2a4578da2781c9e524cb88954e76eb1d91d30419
SekoVper <damehlivingstone1@gmail.com>
2023-02-15 19:55:32 +0000

    Add rental class file

---
+++ rental.rb
@@ -0,0 +1,13 @@
+class Rental
+
+  attr_accessor :date
+
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+    book.rental << self
+    person.rental << self
+  end
+
+end
