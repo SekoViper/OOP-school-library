a9a9749a0d2e98414bb9ef0b3c8b60272eb49f5c
umair-rao <umairmuqeem@gmail.com>
2023-02-21 13:56:55 +0500

    Add process Input

---
+++ process_input.rb
@@ -0,0 +1,18 @@
+class ProcessInput
+  def process_input(input, app)
+    case input
+    when 1
+      app.list_books
+    when 2
+      app.list_people
+    when 3
+      app.create_person
+    when 4
+      app.create_book
+    when 5
+      app.create_rental
+    when 6
+      app.list_rental
+    end
+  end
+end
