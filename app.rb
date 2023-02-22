65ecab4ce5d8c284abcdfb7c041ffe5d0a01089a
SekoVper <damehlivingstone1@gmail.com>
2023-02-22 11:39:40 +0000

    Refactor code to meet the SR principle

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
-require_relative 'create_people'
 require_relative 'create_book'
-require_relative 'create_rental'
+require_relative 'create_people'
+require_relative 'rental_inputs'
 
 class App
+  include CreateBook
+  include CreatePeople
+
   def initialize
     @rentals = []
-    @rental = CreateRental.new
-    @book = CreateBook.new
-    @person = CreatePeople.new
+    @book = Object.new
+    @book.extend CreateBook
+
+    @person = Object.new
+    @person.extend CreatePeople
+
+    @rental = RentalInput.new
   end
 
   def process_input(input, app)
@@ -28,46 +35,11 @@ class App
   end
 
   def create_rental
-    puts 'Person ID: '
-    person_id = gets.chomp.to_i
-    person = @person.people.find { |person1| person1.id == person_id }
-
-    if person.nil?
-      puts "Could not find person with ID: #{person_id}"
-      return
-    end
-
-    puts 'what is the ID of the book being rented?'
-    book_id = gets.chomp.to_i
-    book = @book.books.find { |book1| book1.id == book_id }
-
-    if book.nil?
-      puts "Could not find book with ID: #{book_id}"
-      return
-    end
-
-    puts 'When the book was rented'
-    date = gets.chomp
-
-    book_rented = Rental.new(date, book, person)
-
-    @rentals << book_rented
-    puts "Rented book with ID: #{book_id} to person with ID: #{person_id} on #{date}"
+    @rental.create_rental
   end
 
   def list_rental
-    puts 'What is the ID of the person?'
-    person_id = gets.chomp.to_i
-
-    puts 'Please enter a valid person ID' if person_id.nil?
-
-    list_rentals = @rentals.select { |rental1| rental1.person.id == person_id }
-
-    if list_rentals.empty?
-      puts 'No rentals found'
-      return
-    end
-    list_rentals.each { |rental| puts "Title: #{rental.book.title}, Book Author: #{rental.book.author}" }
+    @rental.list_rental
   end
 
   def list_books
