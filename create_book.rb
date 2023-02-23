18c70ac1da72896462ea3f41f2817fb304b66b15
SekoVper <damehlivingstone1@gmail.com>
2023-02-23 15:02:43 +0000

    add method to read data from JSON file

--- create_book.rb
+++ create_book.rb
@@ -1,15 +1,22 @@
 require_relative 'book'
+require 'json'
 
 module CreateBook
   class << self
     attr_accessor :books
   end
-
-  @books = []
-
-  def self.people
+  
+  def self.books
     @books
   end
+
+  # reading books from json file methods
+  def self.load_books
+    base = "#{Dir.pwd}/saved_data"
+    books_reader = File.read("#{base}/books.json")
+    CreateBook.books = JSON.parse(books_reader).map { |data| Book.new(data['title'], data['author']) } unless books_reader == ''
+  end
+
 
   def list_books
     puts 'No books' if CreateBook.books.empty?
@@ -17,14 +24,14 @@ module CreateBook
       puts "ID: #{book.id} Title: #{book.title}, Author: #{book.author}"
     end
   end
-
+  
   def create_book
     print 'Title: '
     title = gets.chomp
 
     print 'Author: '
     author = gets.chomp
-
+    
     return unless CreateBook.books << Book.new(title, author)
 
     puts 'Book created successfully'
