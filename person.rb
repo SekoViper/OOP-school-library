68418bcdfd3ab5c6fd079d4cbac64091b062779a
SekoVper <damehlivingstone1@gmail.com>
2023-02-24 10:41:52 +0000

    Refactor code

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
   attr_accessor :name, :age, :parent_permission, :rental
   attr_reader :id
 
-  def initialize(age, name, parent_permission: true)
+  def initialize(name, age, parent_permission: true)
     super(name)
     @id = Random.rand(1..1000)
     @name = name
