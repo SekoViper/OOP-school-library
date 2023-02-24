0e28216da407c43a025a5d0b4778f727032144d3
SekoVper <damehlivingstone1@gmail.com>
2023-02-24 12:55:22 +0000

    Refactor code.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,5 @@
-require_relative '../book.rb'
-require_relative '../student.rb'
+require_relative '../book'
+require_relative '../student'
 
 describe Book do
   before :each do
@@ -10,12 +10,4 @@ describe Book do
     expect(@book.author).to eq 'James_clear'
     expect(@book.title).to eq 'Atomic_habits'
   end
-
-  # it 'adds rental to rentals array' do
-  #   person = { 'id' => 12, 'name' => 'Robert', 'age' => 32, 'rentals' => [] }
-  #   person2 = { 'id' => 24, 'name' => 'Albert', 'age' => 52, 'rentals' => [] }
-  #   @book.add_rental('2023-02-08', person)
-  #   # @book.add_rental(person2, '2023-02-08')
-  #   expect(@book.rentals.length).to eql 1
-  # end
 end
